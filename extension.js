24278214beff80beed967a8779a27426c190f44a
Gurjashan <gurjashan-singh@outlook.com>
2024-07-17 23:48:28 -0400

    added function to track when the user types in a javacript file for testing

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
+const { execSync } = require('child_process');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,6 +10,19 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+function getVscodeVersion() {
+  try {
+    const output = execSync('code --version', { encoding: 'utf8' });
+	console.log(output)
+    const version = output.split('\n')[0]; // Get the first line of the output
+    return version;
+  } catch (error) {
+    vscode.window.showErrorMessage('Error getting VS Code version: ' + error.message);
+    return null;
+  }
+}
+
 function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -18,13 +33,23 @@ function activate(context) {
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('css-class-suggester.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from CSS Class Suggester!');
 	});
 
 	context.subscriptions.push(disposable);
+
+	console.log('Extension "typingListener" is now active!');
+
+    let disposable2 = vscode.workspace.onDidChangeTextDocument((event) => {
+        const changes = event.contentChanges;
+        if (changes.length > 0) {
+            const change = changes[0];
+            vscode.window.showInformationMessage(`You typed: ${change.text}`);
+        }
+    });
+
+    context.subscriptions.push(disposable2);
 }
 
 // This method is called when your extension is deactivated
